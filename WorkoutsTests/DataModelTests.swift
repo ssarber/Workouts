f6b40c5788bbf5713e845e0e47d54eb6d8ddf472
ssarber <ssarber@gmail.com>
2016-09-22 11:32:51 -0700

    Synx all the things

--- WorkoutsTests/DataModelTests.swift
+++ WorkoutsTests/DataModelTests.swift
@@ -29,7 +29,7 @@ class DataModelTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-        }sss
+        }
     }
     
 }
